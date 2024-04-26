9b27cc245a0a12b35891634fc22eb1c9506e6b18
huiwoni <151484020+huiwoni@users.noreply.github.com>
2024-04-26 20:56:16 +0900

    Delete src directory

--- src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .WideResNet import *
-from .Hendrycks2020AugMix_ResNeXt import *
-from .officehome_shot import OfficeHome_Shot
-from .domainnet126_shot import DomainNet126_Shot
-from .load_model import load_model
